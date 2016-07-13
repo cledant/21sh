8b5c7b10e52d7ae8c7a08f20d2ecb3072b3412c1
cledant <cledant@student.42.fr>
2016-07-13 15:31:58 +0200

    WIP

--- srcs/ft_is_special_char.c
+++ srcs/ft_is_special_char.c
@@ -6,7 +6,7 @@
 /*   By: cledant <cledant@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/07/13 10:06:23 by cledant           #+#    #+#             */
-/*   Updated: 2016/07/13 11:32:20 by cledant          ###   ########.fr       */
+/*   Updated: 2016/07/13 14:28:57 by cledant          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,17 @@
 
 int		ft_is_special_char(char s[4], t_env *env)
 {
-	if ((s[0] == 10 && s[1] == 0 && s[2] == 0 && s[3] == 0))
+	if ((s[0] == 10 && s[1] == 0 && s[2] == 0 && s[3] == 0)) /*enter*/
 	{
 		write(env->fd_tty, "\n", 1);
 		ft_list_wputendl_fd(env->cur, env->fd_tty);
 		if ((env->hist = ft_lstnewpushback_list(env->hist, env->cur)) == NULL)
 			ft_handler(20000);
 		write(env->fd_tty, "$>", 2);
+		env->cur = NULL;
 		return (1);
 	}
-	if ((s[0] == 4 && s[1] == 0 && s[2] == 0 && s[3] == 0))
+	else if ((s[0] == 4 && s[1] == 0 && s[2] == 0 && s[3] == 0)) /*CTRL + D*/
 		ft_handler(10000);
 	return (0);
 }
