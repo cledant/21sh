9b68499860ab38e0789ccaa88b1b3bef0df8d686
cledant <cledant@student.42.fr>
2016-08-07 19:39:42 +0200

    WIP print_data apres avoir bouger right/left

--- srcs/ft_print_data.c
+++ srcs/ft_print_data.c
@@ -6,7 +6,7 @@
 /*   By: cledant <cledant@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/07/15 14:27:32 by cledant           #+#    #+#             */
-/*   Updated: 2016/08/07 17:54:53 by cledant          ###   ########.fr       */
+/*   Updated: 2016/08/07 19:38:54 by cledant          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,16 @@
 
 void		ft_print_data(char s[4], t_env *env)
 {
-	ft_wputchar_char_fd(s, env->fd_tty);
+	size_t	bak;
+	
+	if (env->cur_char == env->last_char)
+		ft_wputchar_char_fd(s, env->fd_tty);
+	else
+	{
+		bak = env->cur_char;
+		ft_delete_cur_prompt(env);
+		env->cur_char = bak;
+		write(env->fd_tty, "$>", 2);
+		ft_btree_wputstr_fd(env->last->content, env->fd_tty);
+	}
 }
