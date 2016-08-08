0f8ed7faaa0ce6b226880b7ba9fe49a5cb7e635a
cledant <cledant@student.42.fr>
2016-08-08 22:50:46 +0200

    fix du right et left sur plusieurs ligne

--- srcs/ft_cursor_right.c
+++ srcs/ft_cursor_right.c
@@ -6,7 +6,7 @@
 /*   By: cledant <cledant@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/08/07 16:26:09 by cledant           #+#    #+#             */
-/*   Updated: 2016/08/07 18:31:01 by cledant          ###   ########.fr       */
+/*   Updated: 2016/08/08 22:47:16 by cledant          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,17 +21,8 @@ int		ft_cursor_right(t_env *env)
 		return (1);
 	if (env->cur_il->right != NULL)
 		env->cur_il = env->cur_il->right;
-	if (env->cur_char % env->col == env->col)
-	{
-		ft_putstr_fd(env->doo, env->fd_tty);
-		ft_putstr_fd(env->vi, env->fd_tty);
-		while (i_col > 0)
-		{
-			ft_putstr_fd(env->le, env->fd_tty);
-			i_col--;
-		}
-		ft_putstr_fd(env->ve, env->fd_tty);
-	}
+	if (env->cur_char % env->col == env->col - 1)
+		write(env->fd_tty, "\n", 1);
 	else
 		ft_putstr_fd(env->nd, env->fd_tty);
 	env->cur_char++;
