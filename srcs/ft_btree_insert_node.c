4b102f92b08d9195a19f1339fb0685a606d9c295
cledant <cledant@student.42.fr>
2016-08-08 17:30:40 +0200

    prete du 1er char lors d insertion => regler

--- srcs/ft_btree_insert_node.c
+++ srcs/ft_btree_insert_node.c
@@ -6,7 +6,7 @@
 /*   By: cledant <cledant@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/08/07 17:09:05 by cledant           #+#    #+#             */
-/*   Updated: 2016/08/07 18:32:04 by cledant          ###   ########.fr       */
+/*   Updated: 2016/08/08 17:29:10 by cledant          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,18 @@ t_btree		*ft_btree_insert_node(t_env *env, char s[4])
 
 	if ((new = ft_btree_new(s, 4)) == NULL)
 		return (NULL);
-	new->left = env->cur_il;
-	new->right = env->cur_il->right;
-	new->right->left = new;
-	new->left->right = new;
-	if (env->cur_char == 2)
+	if (env->cur_char != 2)
+	{
+		new->left = env->cur_il;
+		new->right = env->cur_il->right;
+		new->right->left = new;
+		new->left->right = new;
+	}
+	else
+	{
 		env->last->content = new;
+		new->right = env->cur_il;
+		env->cur_il->left = new;
+	}
 	return (new);
 }
