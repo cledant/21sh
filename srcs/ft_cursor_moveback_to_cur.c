eff098d46a40ab674301a8a0fcdbf61a5551c19f
cledant <cledant@student.42.fr>
2016-08-09 18:35:56 +0200

    marche mais affichage degeu

--- srcs/ft_cursor_moveback_to_cur.c
+++ srcs/ft_cursor_moveback_to_cur.c
@@ -5,36 +5,54 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: cledant <cledant@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2016/08/08 12:11:53 by cledant           #+#    #+#             */
-/*   Updated: 2016/08/09 17:57:53 by cledant          ###   ########.fr       */
+/*   Created: 2016/08/09 18:19:35 by cledant           #+#    #+#             */
+/*   Updated: 2016/08/09 18:34:33 by cledant          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "21sh.h"
 
-void	ft_cursor_moveback_to_cur(t_env *env)
+static void		ft_max_left_to_nil(t_env *env)
 {
-//	int		count;
-	size_t	cpy_cc;
+	int		i_col;
+	size_t	bak;
 
-//	count = 0;
-	cpy_cc = env->last_char;
-	while (cpy_cc > env->cur_char)
+	bak = env->last_char;
+	while (env->last_char > 0)
 	{
-		if (cpy_cc % env->col == 0)
+		i_col = 0;
+		if (env->last_char % env->col == 0)
 		{
 			ft_putstr_fd(env->up, env->fd_tty);
-//			while (count > env->col)
-//			{
-//				ft_putstr_fd(env->nd, env->fd_tty);
-//				count++;
-//			}
-//			count = 0;
+			ft_putstr_fd(env->vi, env->fd_tty);
+			while (i_col <= env->col)
+			{
+				ft_putstr_fd(env->nd, env->fd_tty);
+				i_col++;
+			}
+			ft_putstr_fd(env->ve, env->fd_tty);
 		}
-//		else if (cpy_cc % env->col == env->col - 1)
-//			write(env->fd_tty, "\n", 1);
 		else
 			ft_putstr_fd(env->le, env->fd_tty);
-		cpy_cc--;
+		env->last_char--;
 	}
+	env->last_char = bak;
+}
+
+void	ft_cursor_moveback_to_cur(t_env *env)
+{
+	size_t	counter;
+
+	ft_putstr_fd(env->vi, env->fd_tty);
+	counter = 0;
+	ft_max_left_to_nil(env);
+	while (counter < env->cur_char)
+	{
+		if (counter % env->col == env->col - 1)
+			write(env->fd_tty, "\n", 1);
+		else
+			ft_putstr_fd(env->nd, env->fd_tty);
+		counter++;
+	}
+	ft_putstr_fd(env->ve, env->fd_tty);
 }
