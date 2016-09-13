e1c391ad9b99bde69c1b40cb94a88498a700edd8
cledant <cledant@student.42.fr>
2016-09-13 20:31:43 +0200

    maj libft + ft_set_pos_col fixed

--- srcs/ft_set_pos_col.c
+++ srcs/ft_set_pos_col.c
@@ -6,13 +6,26 @@
 /*   By: cledant <cledant@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/05 19:59:05 by cledant           #+#    #+#             */
-/*   Updated: 2016/09/11 23:57:28 by cledant          ###   ########.fr       */
+/*   Updated: 2016/09/13 20:16:23 by cledant          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "21sh.h"
 
-void	ft_set_pos_col(t_env *env)
+static void		ft_get_max_rl(t_env *env, t_btree *cmd)
+{
+	char	*s;
+
+	while (cmd != NULL)
+	{
+		s = cmd->content;
+		if (s[0] == '\n' && s[1] == 0 && s[2] == 0 && s[3] == 0)
+			env->max_rl++;
+		cmd = cmd->right;
+	}
+}
+
+void			ft_set_pos_col(t_env *env)
 {
 	t_btree		*cmd;
 	char		*s;
@@ -23,6 +36,12 @@ void	ft_set_pos_col(t_env *env)
 	cmd = env->last->content;
 	if (cmd->content == NULL)
 		return ;
+	if (env->cur_char == 2)
+	{
+		ft_get_max_rl(env, cmd);
+		return ;
+	}
+	env->pos_col++;
 	while (cmd != env->cur_il)
 	{
 		s = cmd->content;
@@ -36,11 +55,7 @@ void	ft_set_pos_col(t_env *env)
 			env->pos_col++;
 		cmd = cmd->right;
 	}
-	while (cmd != NULL)
-	{
-		s = cmd->content;
-		if (s[0] == '\n' && s[1] == 0 && s[2] == 0 && s[3] == 0)
-			env->max_rl++;
-		cmd = cmd->right;
-	}
+	if (env->nb_rl == 0)
+		env->pos_col--;
+	ft_get_max_rl(env, cmd);
 }
