4ef0a2948303725c1247685cec9cb1e9ee3fb151
cledant <cledant@student.42.fr>
2016-09-17 15:37:20 +0200

    line up OK

--- srcs/ft_line_up.c
+++ srcs/ft_line_up.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: cledant <cledant@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2016/08/15 00:11:59 by cledant           #+#    #+#             */
-/*   Updated: 2016/08/15 14:15:15 by cledant          ###   ########.fr       */
+/*   Created: 2016/09/17 15:09:10 by cledant           #+#    #+#             */
+/*   Updated: 2016/09/17 15:36:49 by cledant          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,17 @@
 
 int		ft_line_up(t_env *env)
 {
-	env->cur_char = env->cur_char;
+	size_t		target;
+	size_t		nb_line;
+
+	if ((env->cur_buff / env->col) == 0)
+		return (1);
+	nb_line = env->cur_buff / env->col;
+	if (nb_line - 1 == 0)
+		target = (nb_line - 1) * env->col + env->pos_col + 2;
+	else
+		target = (nb_line - 1) * env->col + env->pos_col;
+	while (env->cur_buff > target && env->cur_buff != 0)
+		ft_cursor_left_buff_no_set(env);
 	return (1);
 }
