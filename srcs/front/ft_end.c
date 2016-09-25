f1e88ba02851a5b9966d6bd8f3fef23345cd3712
cledant <cledant@student.42.fr>
2016-09-25 21:01:38 +0200

    norme ft_end

--- srcs/front/ft_end.c
+++ srcs/front/ft_end.c
@@ -6,34 +6,25 @@
 /*   By: cledant <cledant@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/08/22 15:53:35 by cledant           #+#    #+#             */
-/*   Updated: 2016/09/17 20:14:05 by cledant          ###   ########.fr       */
+/*   Updated: 2016/09/25 21:00:03 by cledant          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "shell.h"
 
-int		ft_end(t_env *env)
+static inline void		ft_display(t_env *env)
 {
-	char	*s;
+	ft_move_cursor_from_cur_buff_to_before_prompt(env);
+	ft_create_buffer(env);
+	ft_print_buffer(env);
+	ft_move_cursor_from_last_buff_to_cur_buff(env);
+	ft_set_pos_col(env);
+}
+
+static inline void		ft_while(t_env *env, char *s)
+{
 	t_btree	*next;
 
-	if (env->cur_il->content == NULL || (env->cur_char != 2
-		&& env->cur_il->right == NULL))
-		return (1);
-	if (env->cur_char != 2)
-		env->cur_il = env->cur_il->right;
-	env->cur_char++;
-	s = env->cur_il->content;
-	if (env->cur_il == env->last->content && (s[0] == '\n' && s[1] == 0
-		&& s[2] == 0 && s[3] == 0))
-	{
-		ft_move_cursor_from_cur_buff_to_before_prompt(env);
-		ft_create_buffer(env);
-		ft_print_buffer(env);
-		ft_move_cursor_from_last_buff_to_cur_buff(env);
-		ft_set_pos_col(env);
-		return (1);
-	}
 	while (1)
 	{
 		if (env->cur_il->right == NULL)
@@ -52,10 +43,26 @@ int		ft_end(t_env *env)
 			env->cur_char++;
 		env->cur_il = env->cur_il->right;
 	}
-	ft_move_cursor_from_cur_buff_to_before_prompt(env);
-	ft_create_buffer(env);
-	ft_print_buffer(env);
-	ft_move_cursor_from_last_buff_to_cur_buff(env);
-	ft_set_pos_col(env);
+}
+
+int						ft_end(t_env *env)
+{
+	char	*s;
+
+	if (env->cur_il->content == NULL || (env->cur_char != 2
+		&& env->cur_il->right == NULL))
+		return (1);
+	if (env->cur_char != 2)
+		env->cur_il = env->cur_il->right;
+	env->cur_char++;
+	s = env->cur_il->content;
+	if (env->cur_il == env->last->content && (s[0] == '\n' && s[1] == 0
+		&& s[2] == 0 && s[3] == 0))
+	{
+		ft_display(env);
+		return (1);
+	}
+	ft_while(env, s);
+	ft_display(env);
 	return (1);
 }
